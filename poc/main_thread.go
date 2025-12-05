82d167f6fc8e5382863e24af8cfaf800ff775513
codedude <codedude@users.noreply.github.com>
2025-12-05 09:51:53 -0500

    try multithread file read

---
+++ poc/main_thread.go
@@ -0,0 +1,67 @@
+// Try to read file with multiple thread using seek
+// result => not better, sometimes even worse
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+)
+
+const FILE_CHUNK_SIZE = 1024 * 1024 * 4
+const NUM_THREADS = 1
+
+func run(args []string) int {
+	if len(args) < 2 {
+		fmt.Println("Usage: ./exe input")
+		return 0
+	}
+	input := args[1]
+
+	f, _ := os.OpenFile(input, os.O_RDONLY, 0o764)
+	var size int64
+	if info, err := f.Stat(); err == nil {
+		size = info.Size()
+	} else {
+		return -1
+	}
+	fmt.Println("File size: ", size)
+	f.Close()
+
+	// f, _ = os.OpenFile(input, os.O_RDONLY, 0o764)
+	// buffer := make([]byte, FILE_CHUNK_SIZE)
+	// for {
+	// 	n, err := f.Read(buffer)
+	// 	if err != nil || n == 0 {
+	// 		break
+	// 	}
+	// }
+
+	var wg sync.WaitGroup
+	for i := range NUM_THREADS {
+		wg.Add(1)
+		go func(offset int, blockSize int64) {
+			defer wg.Done()
+			inFs, _ := os.OpenFile(input, os.O_RDONLY, 0o764)
+			defer inFs.Close()
+			buff := make([]byte, FILE_CHUNK_SIZE)
+			inFs.Seek(int64(offset)*blockSize, 0)
+			var totalRead int64 = 0
+			for totalRead < blockSize-1 {
+				n, err := inFs.Read(buff)
+				if err != nil {
+					fmt.Println(err)
+				}
+				totalRead += int64(n)
+				// fmt.Printf("T%d = %d\n", offset, totalRead)
+			}
+		}(i, size/NUM_THREADS)
+	}
+	wg.Wait()
+	return 0
+}
+
+func main() {
+	ret := run(os.Args)
+	os.Exit(ret)
+}
